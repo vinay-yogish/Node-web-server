2a06d85c0b816316cac8c1cd672ffb7f6009c710
Vinayr-github <vinayr2096@gmail.com>
2018-10-27 13:51:36 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('message', (message) => {
+    return message;
+});
+
+app.use( (req, res, next) => {
+    let now = new Date().toString();
+
+    const log = `${now} : ${req.method} ${req.url} \n`;
+    fs.appendFileSync('server-log.log', log);
+
+    next ();
+} );
+
+// app.use( (req, res, next) => {
+
+//     res.render('maintainace');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    
+    res.render('home.hbs', {
+        pageTitle:  'Home Page',
+        message: 'Welcome to the Website'
+    });
+    
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+
+});
+
+app.get('/bad', (req, res) => {
+    res.send('A Bad Request!');
+});
+
+app.listen(3000, () => {
+    console.log('Server has started at port 3000');
+});
